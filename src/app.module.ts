749f93a5db345f0c8ed6bd3ef6d420c6faaac763
Alexis MF <alexismf2802@gmail.com>
2023-07-22 15:10:33 -0500

    change DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alexismf2802:6Dz66x3WkHmUgKOF@tasksbd.cxmwogd.mongodb.net/'),
+  imports: [MongooseModule.forRoot('mongodb+srv://alexismf2802:4ZvZB8lejJLbjyzk@tasksbd.3qzyn6e.mongodb.net/'),
   TasksModule],  
 })
 export class AppModule {}
