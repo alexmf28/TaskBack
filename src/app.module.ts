b275eca1648ac94e52d70dfc5ce8d9182cae5ee0
Alexis MF <alexismf2802@gmail.com>
2023-07-21 21:32:38 -0500

    Change BD connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alexismf2802:alx280220.@tasksbd.1abk6ly.mongodb.net/'),
+  imports: [MongooseModule.forRoot('mongodb+srv://alexismf2802:6Dz66x3WkHmUgKOF@tasksbd.cxmwogd.mongodb.net/'),
   TasksModule],  
 })
 export class AppModule {}
